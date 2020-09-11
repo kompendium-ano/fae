41f537f260065d4ca40efa110a8d8c334e25d557
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2020-09-11 19:02:07 +0300

    remove useless files

--- ethereum/relayer/target/rls/debug/deps/relayer-cefd9ec51d69b605.d
+++
@@ -1,5 +0,0 @@
-/home/sigrlami9/work/projects-blockchain/fae/ethereum/relayer/target/rls/debug/deps/relayer-cefd9ec51d69b605.rmeta: src/main.rs
-
-/home/sigrlami9/work/projects-blockchain/fae/ethereum/relayer/target/rls/debug/deps/relayer-cefd9ec51d69b605.d: src/main.rs
-
-src/main.rs:
