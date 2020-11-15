b3dfd96fe2ab0993fb2648b8077b2f322a289890
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2020-11-15 21:08:10 +0200

    add contexts for the Web3 Provider; basic constants

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <Home></Home>
       </header>
     </div>
   );
