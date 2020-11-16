8dc57f16d090d69804fc9fb240ca8f6d8a0a2970
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2020-11-16 11:15:49 +0200

    wrap everything in web3 provider; update main look

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import { Web3Provider } from './contexts/Web3Context'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Home></Home>
+        <Web3Provider>
+          <Home></Home>
+        </Web3Provider>
       </header>
     </div>
   );
---
+++ web/src/contexts/BridgeContext.tsx
@@ -0,0 +1,1 @@
+import React, { useCallback, useEffect, useState } from 'react';
