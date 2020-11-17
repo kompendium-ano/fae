319a6ab2819e8458b342fe2fc28bedd30a1319cc
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2020-11-17 10:20:36 +0200

    refactor to smaller components; add from/to token

---
+++ web/src/components/FromToken.tsx
@@ -0,0 +1,10 @@
+
+  import React, { useContext, useEffect, useState } from 'react';
+  
+  import DropDown from '../assets/drop-down.svg';
+  import { BridgeContext } from '../contexts/BridgeContext';
+  import { Web3Context } from '../contexts/Web3Context';
+
+  
+  export const FromToken = () => {
+  };
