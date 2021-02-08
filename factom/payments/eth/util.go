359cdd5eeecffd7edb210b825bea89ee7d169ffa
Max Buglakov <max.buglakov@gmail.com>
2021-02-08 13:45:27 +0200

    add function to verify signature

--- factom/payments/eth/util.go
+++ factom/payments/eth/util.go
@@ -18,3 +18,36 @@ func NormalizeAddress(s string) string {
 	}
 	return s
 }
+
+// used for tests
+//
+//func main() {
+//	fmt.Println(verifySig(
+//		"0x424314B6E4dfeC4b703F2c6fDba28F1724094D11",
+//		"0x..", // signature hex
+//		[]byte("kompendium"), //message
+//	))
+//}
+
+func VerifySig(from, sigHex string, msg []byte) bool {
+	fromAddr := common.HexToAddress(from)
+
+	sig := hexutil.MustDecode(sigHex)
+	// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L442
+	if sig[64] == 27 || sig[64] == 28 {
+		sig[64] -= 27
+	}
+
+	if sig[64] != 0 && sig[64] != 1 {
+		return false
+	}
+
+	pubKey, err := crypto.SigToPub(SignHash(msg), sig)
+	if err != nil {
+		return false
+	}
+
+	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
+
+	return fromAddr == recoveredAddr
+}
